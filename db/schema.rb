2f6b766a8b2a75883efaf8b453db4640be9116de
Stephen Clem <stevebclem@gmail.com>
2015-03-29 18:54:37 -0700

    converting api into database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150328001105) do
+ActiveRecord::Schema.define(version: 20150329164809) do
+
+  create_table "trails", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "type"
+    t.float    "rating"
+    t.string   "description"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
