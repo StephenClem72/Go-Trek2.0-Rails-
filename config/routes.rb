c0394fbffd2f0457793b804b4b22bdbaf7ee13a5
Stephen Clem <stevebclem@gmail.com>
2015-04-02 20:59:57 -0700

    excluded routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   resources :trails do
-    resources :comments, except: [:edit, :new]
+    resources :comments, except: [:edit, :new, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
